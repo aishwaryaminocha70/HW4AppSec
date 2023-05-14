3539c3f101cab76baa1256fac6e5183628a2a1e5
Aishwarya Minocha <am8569@nyu.edu>
2023-05-14 12:47:01 -0400

    API

--- app/src/main/java/com/example/giftcardsite/CardScrollingActivity.kt
+++ app/src/main/java/com/example/giftcardsite/CardScrollingActivity.kt
@@ -20,8 +20,9 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-class CardScrollingActivity : AppCompatActivity(){
-    private var loggedInUser : User? = null
+class CardScrollingActivity : AppCompatActivity() {
+    private var loggedInUser: User? = null
+
     // private lateinit var sensorManager: SensorManager
     // private var mAccel: Sensor? = null;
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,16 +38,18 @@ class CardScrollingActivity : AppCompatActivity(){
         setContentView(R.layout.activity_scrolling)
         setSupportActionBar(findViewById(R.id.toolbar))
         loggedInUser = intent.getParcelableExtra<User>("User")
-        var button : Button = findViewById<Button>(R.id.view_cards_button)
+        var button: Button = findViewById<Button>(R.id.view_cards_button)
         button.text = "View Products"
         button.setOnClickListener {
-            val intent = Intent(this, ProductScrollingActivity::class.java).apply{
+            val intent = Intent(this, ProductScrollingActivity::class.java).apply {
                 putExtra("User", loggedInUser)
             }
             startActivity(intent)
         }
-        var builder: Retrofit.Builder = Retrofit.Builder().baseUrl("https://appsec.moyix.net").addConverterFactory(
-            GsonConverterFactory.create())
+        var builder: Retrofit.Builder =
+            Retrofit.Builder().baseUrl("https://appsec.moyix.net").addConverterFactory(
+                GsonConverterFactory.create()
+            )
         var retrofit: Retrofit = builder.build()
         var client: CardInterface = retrofit.create(CardInterface::class.java)
         val outerContext = this
@@ -60,6 +63,7 @@ class CardScrollingActivity : AppCompatActivity(){
                 Log.d("Product Failure", "Product Failure in onFailure")
                 Log.d("Product Failure", t.message.toString())
             }
+
             override fun onResponse(call: Call<List<Card?>?>, response: Response<List<Card?>?>) {
                 if (!response.isSuccessful) {
                     Log.d("Product Failure", "Product failure. Yay.")
@@ -70,71 +74,16 @@ class CardScrollingActivity : AppCompatActivity(){
                     Log.d("Product Failure", "Parsed null product list")
                     Log.d("Product Failure", response.toString())
                 } else {
-                    recyclerView.adapter = CardRecyclerViewAdapter(outerContext, cardListInternal, loggedInUser)
+                    recyclerView.adapter =
+                        CardRecyclerViewAdapter(outerContext, cardListInternal, loggedInUser)
                 }
             }
         })
     }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
-    /*override fun onLocationChanged(location: Location) {
-        var userInfoContainer = UserInfoContainer(location, null, loggedInUser?.token)
-        var builder: Retrofit.Builder = Retrofit.Builder().baseUrl("https://appsec.moyix.net").addConverterFactory(
-            GsonConverterFactory.create())
-        var retrofit: Retrofit = builder.build()
-        var client: UserInfo = retrofit.create(UserInfo::class.java)
-        client.postInfo(userInfoContainer, loggedInUser?.token)?.enqueue(object: Callback<User?> {
-            override fun onFailure(call: Call<User?>, t: Throwable) {
-                Log.d("Metric Failure", "Metric Failure in onFailure")
-                Log.d("Metric Failure", t.message.toString())
-            }
-            override fun onResponse(call: Call<User?>, response: Response<User?>) {
-                if (!response.isSuccessful) {
-                    Log.d("Metric Failure", "Metric failure. Yay.")
-                } else {
-                    Log.d("Metric Success", "Metric success. Boo.")
-                    Log.d("Metric Success", "Token:${userInfoContainer.token}")
-                }
-            }
-        })
-    }
-    override fun onSensorChanged(event: SensorEvent?) {
-        if (event != null) {
-            var userInfoContainer = UserInfoContainer(null, event.values[0].toString(), loggedInUser?.token)
-            var builder: Retrofit.Builder = Retrofit.Builder().baseUrl("https://appsec.moyix.net").addConverterFactory(
-                GsonConverterFactory.create())
-            var retrofit: Retrofit = builder.build()
-            var client: UserInfo = retrofit.create(UserInfo::class.java)
-            client.postInfo(userInfoContainer, loggedInUser?.token)?.enqueue(object: Callback<User?> {
-                override fun onFailure(call: Call<User?>, t: Throwable) {
-                    Log.d("Metric Failure", "Metric Failure in onFailure")
-                    Log.d("Metric Failure", t.message.toString())
-                }
-                override fun onResponse(call: Call<User?>, response: Response<User?>) {
-                    if (!response.isSuccessful) {
-                        Log.d("Metric Failure", "Metric failure. Yay.")
-                    } else {
-                        Log.d("Metric Success", "Metric success. Boo.")
-                        Log.d("Metric Success", "Token:${userInfoContainer.token}")
-                    }
-                }
-            })
-        }
-    }*/
-    /*override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        return
-    }
-    override fun onResume() {
-        super.onResume()
-        /*mAccel?.also { accel ->
-            sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL)
-        }*/
-    }
-    override fun onPause() {
-        super.onPause()
-        //sensorManager.unregisterListener(this)
-    }*/
 }
