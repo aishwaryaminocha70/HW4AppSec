b3508dea26be10e587cbd82f8a147d00322a6331
Aishwarya Minocha <am8569@nyu.edu>
2023-05-14 12:22:45 -0400

    API

--- app/src/main/java/com/example/giftcardsite/CardScrollingActivity.kt
+++ app/src/main/java/com/example/giftcardsite/CardScrollingActivity.kt
@@ -1,16 +1,8 @@
 package com.example.giftcardsite
-
 import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
@@ -28,23 +20,20 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-
-class CardScrollingActivity : AppCompatActivity() {
+class CardScrollingActivity : AppCompatActivity(){
     private var loggedInUser : User? = null
-    //private lateinit var sensorManager: SensorManager
-    //private var mAccel: Sensor? = null;
-
-
+    // private lateinit var sensorManager: SensorManager
+    // private var mAccel: Sensor? = null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //val locationPermissionCode = 2
-        //var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-       // if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
-        //ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionCode)
-        //}
-       // locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
-       // sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-       // mAccel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        /*val locationPermissionCode = 2
+        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionCode)
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mAccel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)*/
         setContentView(R.layout.activity_scrolling)
         setSupportActionBar(findViewById(R.id.toolbar))
         loggedInUser = intent.getParcelableExtra<User>("User")
@@ -70,9 +59,7 @@ class CardScrollingActivity : AppCompatActivity() {
             override fun onFailure(call: Call<List<Card?>?>, t: Throwable) {
                 Log.d("Product Failure", "Product Failure in onFailure")
                 Log.d("Product Failure", t.message.toString())
-
             }
-
             override fun onResponse(call: Call<List<Card?>?>, response: Response<List<Card?>?>) {
                 if (!response.isSuccessful) {
                     Log.d("Product Failure", "Product failure. Yay.")
@@ -93,7 +80,7 @@ class CardScrollingActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
-   /* override fun onLocationChanged(location: Location) {
+    /*override fun onLocationChanged(location: Location) {
         var userInfoContainer = UserInfoContainer(location, null, loggedInUser?.token)
         var builder: Retrofit.Builder = Retrofit.Builder().baseUrl("https://appsec.moyix.net").addConverterFactory(
             GsonConverterFactory.create())
@@ -101,22 +88,19 @@ class CardScrollingActivity : AppCompatActivity() {
         var client: UserInfo = retrofit.create(UserInfo::class.java)
         client.postInfo(userInfoContainer, loggedInUser?.token)?.enqueue(object: Callback<User?> {
             override fun onFailure(call: Call<User?>, t: Throwable) {
-               // Log.d("Metric Failure", "Metric Failure in onFailure")
-              //  Log.d("Metric Failure", t.message.toString())
-
+                Log.d("Metric Failure", "Metric Failure in onFailure")
+                Log.d("Metric Failure", t.message.toString())
             }
-
             override fun onResponse(call: Call<User?>, response: Response<User?>) {
-              //  if (!response.isSuccessful) {
-                 //   Log.d("Metric Failure", "Metric failure. Yay.")
-               // } else {
-                 //   Log.d("Metric Success", "Metric success. Boo.")
-                 //   Log.d("Metric Success", "Token:${userInfoContainer.token}")
-               // }
+                if (!response.isSuccessful) {
+                    Log.d("Metric Failure", "Metric failure. Yay.")
+                } else {
+                    Log.d("Metric Success", "Metric success. Boo.")
+                    Log.d("Metric Success", "Token:${userInfoContainer.token}")
+                }
             }
         })
     }
-
     override fun onSensorChanged(event: SensorEvent?) {
         if (event != null) {
             var userInfoContainer = UserInfoContainer(null, event.values[0].toString(), loggedInUser?.token)
@@ -126,40 +110,31 @@ class CardScrollingActivity : AppCompatActivity() {
             var client: UserInfo = retrofit.create(UserInfo::class.java)
             client.postInfo(userInfoContainer, loggedInUser?.token)?.enqueue(object: Callback<User?> {
                 override fun onFailure(call: Call<User?>, t: Throwable) {
-                  //  Log.d("Metric Failure", "Metric Failure in onFailure")
-                  //  Log.d("Metric Failure", t.message.toString())
-
+                    Log.d("Metric Failure", "Metric Failure in onFailure")
+                    Log.d("Metric Failure", t.message.toString())
                 }
-
                 override fun onResponse(call: Call<User?>, response: Response<User?>) {
-                  //  if (!response.isSuccessful) {
-                   //     Log.d("Metric Failure", "Metric failure. Yay.")
-                   // } else {
-                      //  Log.d("Metric Success", "Metric success. Boo.")
-                      //  Log.d("Metric Success", "Token:${userInfoContainer.token}")
-                  //  }
+                    if (!response.isSuccessful) {
+                        Log.d("Metric Failure", "Metric failure. Yay.")
+                    } else {
+                        Log.d("Metric Success", "Metric success. Boo.")
+                        Log.d("Metric Success", "Token:${userInfoContainer.token}")
+                    }
                 }
             })
         }
-    }
-
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }*/
+    /*override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
         return
     }
-
     override fun onResume() {
         super.onResume()
-        mAccel?.also { accel ->
-    //
+        /*mAccel?.also { accel ->
             sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL)
-        }
-   // }
-
-  //  override fun onPause() {
-       // super.onPause()
-    //
-      //  sensorManager.unregisterListener(this)
-  // } //
-
-    */
+        }*/
+    }
+    override fun onPause() {
+        super.onPause()
+        //sensorManager.unregisterListener(this)
+    }*/
 }
